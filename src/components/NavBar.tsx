3b94a9b195ab2dad7aeb79518e5dad1e4257b737
Aaron Yabut <aaronjiego@yahoo.com>
2022-12-26 17:20:20 -0800

    made the same changes to another picture, and fixed the resume link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start ">
               <a className="flex flex-shrink-0 items-center scroll-smooth" href="#">
-                <img className="hidden h-14 w-auto lg:block" src="/logoPic.png" alt="logo" />
+                <img className="hidden h-14 w-auto lg:block" src="https://raw.githubusercontent.com/Aaronyabut/portfolio/main/logoPic.png" alt="logo" />
               </a>
             </div>
 
