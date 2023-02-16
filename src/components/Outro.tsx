c5a56cc18efd2e92a344c683a38a238cad1eeb2c
Aaron Yabut <aaronjiego@yahoo.com>
2023-02-15 17:32:32 -0800

    styling portfolio modal

--- src/components/Outro.tsx
+++ src/components/Outro.tsx
@@ -37,7 +37,7 @@ export default function Example() {
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -47,7 +47,7 @@ export default function Example() {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-blueish text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-blueish text-left shadow-xl transition-all sm:my-8 sm:w-6/12 sm:max-w-sm w-8/12">
                   <div className="flex flex-row-reverse mt-4 mr-6">
                     <button onClick={()=> setOpen(false)}>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
@@ -58,17 +58,25 @@ export default function Example() {
 
                   <div className="bg-blueish px-4 pb-6 sm:px-6 sm:pb-6">
                     <div className="sm:flex sm:items-start">
-                      <div className="mt-1 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <div className="mt-1 text-left sm:mt-0 sm:ml-4 sm:text-left">
                         <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-white">
                           Portfolio Information
                         </Dialog.Title>
                         <div className="mt-2">
-                          <p className="text-md text-white">
+                          <ul className="text-md text-white">
+                            This portfolio is built using:
+                            <li className="ml-4">&#x2022;&nbsp;<a id="link" href="https://www.typescriptlang.org/docs/handbook/intro.html" target="_blank" className="inline-block">TypeScript</a></li>
+                            <li className="ml-4">&#x2022;&nbsp;<a id="link" href="https://www.reactjs.org/" target="_blank" className="inline-block">React.js</a></li>
+                            <li className="ml-4">&#x2022;&nbsp;<a id="link" href="https://tailwindcss.com/" target="_blank" className="inline-block">TailwindCSS</a></li>
+                            <li className="ml-4">&#x2022;&nbsp;<a id="link" href="https://vitejs.dev/guide/" target="_blank" className="inline-block">Vite</a></li>
+                          </ul>
+                          {/* <p className="text-md text-white">
                             This portfolio is built using&nbsp;
                             <a id="link" href="https://www.typescriptlang.org/docs/handbook/intro.html" target="_blank" className="inline-block">TypeScript</a>,&nbsp;
-                            <a id="link" href="https://www.reactjs.org/" target="_blank" className="inline-block">React.js</a>, and&nbsp;
-                            <a id="link" href="https://tailwindcss.com/" target="_blank" className="inline-block">TailwindCSS</a>.
-                          </p>
+                            <a id="link" href="https://www.reactjs.org/" target="_blank" className="inline-block">React.js</a>,&nbsp;
+                            <a id="link" href="https://tailwindcss.com/" target="_blank" className="inline-block">TailwindCSS</a>, and&nbsp;
+                            <a id="link" href="https://vitejs.dev/guide/" target="_blank" className="inline-block">Vite</a>.
+                          </p> */}
                         </div>
                       </div>
                     </div>
