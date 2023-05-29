cd505eac0f2b512fd365d83d92730de00a83ceaa
Aaron Yabut <aaronjiego@yahoo.com>
2023-05-28 23:52:41 -0700

    joined paragraphs

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,11 +14,7 @@ export default function AboutMe () {
               <h3 className="text-left text-1xl font-display text-white">
                 I'm a graduate student at&nbsp;
                 <a href="https://www.csulb.edu/about-csulb/our-mission-vision-values/" id="link" className="text-pinkish inline-block" target="_blank" rel="noreferrer">California State University of Long Beach</a>
-                , holding a B.S. in Kinesiology. Originally, my path was set towards a career in Physical Therapy, but everything changed during my final year of college when I stumbled upon the world of software development. With just a few lines of code, I was immediately captivated by its allure. The logical thinking required and the opportunity to unleash my creativity resonated with me on a deep level.
-              </h3>
-              <>&nbsp;</>
-              <h3 className="text-left text-1xl font-display text-white">
-                Motivated by this newfound passion, I embarked on a self-guided journey to teach myself how to code. Immersed in the world of programming, I devoted most of my time to honing my skills after graduating from university.
+                , holding a B.S. in Kinesiology. Originally, my path was set towards a career in Physical Therapy, but everything changed during my final year of college when I stumbled upon the world of software development. With just a few lines of code, I was immediately captivated by its allure. The logical thinking required and the opportunity to unleash my creativity resonated with me on a deep level. Motivated by this newfound passion, I embarked on a self-guided journey to teach myself how to code. Immersed in the world of programming, I devoted most of my time to honing my skills after graduating from university.
               </h3>
               <>&nbsp;</>
               <h3 className="text-left text-1xl font-display text-white">
